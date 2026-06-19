6eb93c700a4aec7faa594efce4a2b3b6a33d4f15
KwakuJO <kwakujotchere@gmail.com>
2026-06-19 12:08:51 -0600

    First Commit
    Added all necessary Next files
    Added StudentCard component
    Cleared page.tsx unnecessary code

---
+++ app/components/StudentCard.tsx
@@ -0,0 +1,5 @@
+const StudentCardComponent = () => {
+
+}
+
+export default StudentCardComponent;
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import StudentCardComponent from "./components/StudentCard";
+
+
+export default function Home() {
+  return (
+    <div className="">
+      <main className="">
+        
+      </main>
+    </div>
+  );
+}
