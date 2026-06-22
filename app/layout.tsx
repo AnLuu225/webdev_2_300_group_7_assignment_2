587da7ab91eadcf860670ef64b3e6b8ac8517587
valensasa16 <139813256+valensasa16@users.noreply.github.com>
2026-06-22 17:01:20 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,11 @@ export default function RootLayout({
     <html
       lang="en"
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-screen flex flex-col">
+  <Navbar />
+  {children}
+  <Footer />
+</body>
     </html>
   );
 }
