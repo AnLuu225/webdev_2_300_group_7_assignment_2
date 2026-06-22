f1704609dcd937a714ff4d222e650c12f30eb914
valensasa16 <139813256+valensasa16@users.noreply.github.com>
2026-06-22 16:45:38 -0600

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,9 @@
 export default function Footer() {
   return (
-    <footer>
-      <hr />
-      <p>South Casserole High School</p>
-      <p>Calgree, Elbarte</p>
+    <footer className="w-full bg-gray-900 text-white text-center py-5 mt-10">
+      <p className="text-sm">
+        © 2026 Student Manager. Created by Valentina Gil.
+      </p>
     </footer>
   );
 }
